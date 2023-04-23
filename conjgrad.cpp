00d972088f38f75fbc60c55df556de10eda72588
Alan Herculano Diniz <alanhdiniz@gmail.com>
2023-04-23 17:24:25 -0300

    Implementing the conjugate gradient method in C++

--- conjgrad.cpp
+++ conjgrad.cpp
@@ -1,4 +1,5 @@
 #include "octave/oct.h"
+#include "octave/parse.h"
 #include <cmath>
 
 inline double dot_product(const ColumnVector &a, const ColumnVector &b)
@@ -16,63 +17,103 @@ inline double euc_norm(const ColumnVector &a)
 
 DEFUN_DLD(conjgrad, args, nargout, "C++ implementation of the Conjugate Gradient method.")
 {
+    octave_value_list retval(nargout);
+
     Matrix A = args(0).matrix_value();
     ColumnVector b = args(1).column_vector_value();
     double tol = args(2).double_value();
     int max_iter = args(3).int_value();
+    
+    const double EPS = octave::feval("eps")(0).double_value();
+
+    if (tol >= 1.0)
+        warning("Input tol is bigger than 1. \n Try to use a smaller tolerance.");
+    else if (tol <= EPS / 2.0)
+        warning("Input tol may not be achievable by pcg. \n Try to use a bigger tolerance.");
 
     int n = A.rows();
+    double b_norm = euc_norm(b);
 
-    ColumnVector x0(n);
+    ColumnVector x(n); // Solution
     for (int i = 0; i < n; ++i)
-        x0(i) = 0.0;
-    ColumnVector d0 = b;
-    ColumnVector r0 = b;
-    ColumnVector x(n);
-    ColumnVector d(n);
-    ColumnVector r(n);
-
-    double delta = dot_product(r0, r0);
-    double delta0 = delta;
-    double epsilon = tol * tol * delta0;
-
-    int iter = 0;
-    int flag = 0;
-
-    ColumnVector resvec(max_iter);
+        x(i) = 0.0;
+    ColumnVector x_pr(n); // Approximation in previous iteration
+    ColumnVector x_min(n); // Approximation with minimum residual
+    
+    int flag = 0; // Convergence flag
+    
+    double relres = 0.0; // Relative residual by the end of the algorithm
+    
+    int iter = 0, iter_min = 0; // Number of iterations made and iteration with minimum residual
+    
+    ColumnVector resvec(max_iter); // Residuals' norms calculated in each iteration
     for (int i = 0; i < max_iter; ++i)
         resvec(i) = 0.0;
 
-    while (delta < epsilon && iter < max_iter)
+    if (b_norm == 0.0)
     {
-        ColumnVector v = A * d0;
-        double lambda = delta / dot_product(d0, v);
-        x = x0 + lambda * d0;
-        r = r0 - lambda * v;
-        resvec(iter) = euc_norm(r);
-        delta0 = delta;
-        delta = dot_product(r, r);
-        double beta = delta0 / delta;
-        d = r + beta * d0;
-        x0 = x;
-        r0 = r;
-        d0 = d;
+        printf("The right hand side vector is all zero so pcg \n");
+        printf("returned an all zero solution without iterating.\n");
+        retval(0) = x;
+        retval(1) = flag;
+        retval(2) = relres;
+        retval(3) = iter;
+        retval(4) = resvec;
+        return retval;
+    }
+
+    x_pr = x_min = x;
+
+    ColumnVector r = b; // Residual vector
+    resvec(0) = b_norm;
+    
+    // Auxiliar variables:
+    double tau, old_tau, alpha, old_alpha;
+    alpha = old_tau = 1.0;
+
+    ColumnVector p(n);
+    for (int i = 0; i < n; ++i)
+        p(i) = 0.0;
+    
+    while (resvec(iter) > tol * b_norm && iter < max_iter)
+    {
+        tau = dot_product(r, r);
+        resvec(iter) = sqrt(tau);
+        double beta = tau / old_tau;
+        old_tau = tau;
+        
+        p = r + beta * p;
+        
+        ColumnVector w = A * p;
+        
+        old_alpha = alpha;
+        alpha = tau / dot_product(p, w);
+
+        x += alpha * p;
+        r -= alpha * w;
+
+        if (resvec(iter) <= resvec(iter_min))
+        {
+            x_min = x;
+            iter_min = iter;
+        }
+
         ++iter;
 
-        if (euc_norm(x - x0) <= epsilon * euc_norm(x))
+        if (euc_norm(x - x_pr) <= EPS * euc_norm(x))
         {
             flag = 3;
             break;
         }
+
+        x_pr = x;
     }
 
-    if (iter >= max_iter)
-        flag = 1;
+    flag = (flag != 3 && iter >= max_iter) ? 1 : flag;
 
-    octave_value_list retval(nargout);
     retval(0) = x;
     retval(1) = flag;
-    retval(2) = resvec(iter - 1);
+    retval(2) = relres;
     retval(3) = iter;
     retval(4) = resvec;
     return retval;
